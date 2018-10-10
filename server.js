aa1ec1111afec936e6d347dd81e3c40180677914
John <john@Johns-MacBook-Pro.local>
2018-10-10 11:44:23 -0500

    ironed out server/router kinks and set up index.ejs

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+const robotsController = require('./controllers/robots');
+
+app.use('/robots', robotsController);
+
 app.listen(3000, () => {
 	console.log('listening at 3000');
 })
+
+
 

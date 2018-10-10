2284a28750def084321a604107acdf8c1d9b8a12
John <john@Johns-MacBook-Pro.local>
2018-10-10 12:20:17 -0500

    edit route & ejs built and work

--- controllers/robots.js
+++ controllers/robots.js
@@ -3,15 +3,31 @@ const router = express.Router();
 
 const Robots = require('../models/robots');
 
+//index route
 router.get('/', (req, res) => {
 	res.render('index.ejs', {
 		robots: Robots
 	})
 });
 
+//delete route
 router.delete('/:id', (req, res) => {
 	Robots.splice([req.params.id], 1)
 	res.redirect('/robots')
 });
+
+//edit route
+router.get('/:id/edit', (req, res) => {
+	res.render('edit.ejs', {
+		robot: Robots[req.params.id],
+		id: req.params.id
+	})
+})
+
+//edit
+router.put('/:id/', (req, res) => {
+	Robots[req.params.id] = req.body;
+	res.redirect('/robots')
+})
 
 module.exports = router;

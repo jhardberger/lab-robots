e23fc1e489ceae9809c9d2be57394f605ee2eb35
John <john@Johns-MacBook-Pro.local>
2018-10-10 11:30:20 -0500

    Set up server & file structure, built first model

---
+++ models/robots.js
@@ -0,0 +1,19 @@
+const robots = [
+	{
+		name: 'Astroboy',
+		origin: 'Astroboy'
+	},
+	{
+		name: 'Hal',
+		origin: '2001'
+	},
+	{
+		name: 'R2D2',
+		origin: 'Star Wars'
+	},
+	{
+		name: 'Borg',
+		origin: 'Star Trek'
+	}
+
+]
